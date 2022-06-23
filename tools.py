dc6e165db72244ab56bf7fccd2622bd186e177d4
Vladimir Petrigo <vladimir.petrigo@gmail.com>
2022-06-24 01:56:44 +0300

    tools: Update generator tool

--- tools.py
+++ tools.py
@@ -1,18 +1,18 @@
 # -*- coding: utf-8 -*-
+import argparse
 import asyncio
 import dataclasses
 import multiprocessing
 import pathlib
+import re
 import shutil
-import subprocess
 import tempfile
-import time
 from typing import Union, Iterable, Dict, Any
 
 import toml
 
 LICENSE = "BSD-3-Clause"
-VERSION = "0.0.1"
+VERSION = "0.1.0"
 REPOSITORY = "https://github.com/vpetrigo/efm32-pacs"
 AUTHORS = ["Vladimir Petrigo <vladimir.petrigo@gmail.com>"]
 SVD_DIR = pathlib.Path(".", "svd").resolve()
@@ -29,7 +29,7 @@ class SvdMeta:
 def mcu_crate_toml_template(svd_descr: SvdMeta) -> Dict[str, Any]:
     cargo_toml_mcu_template = {
         "package": {
-            "name": svd_descr.name,
+            "name": f"{svd_descr.name}-pac",
             "description": f"Peripheral access API for {svd_descr.name.upper()} MCU (generated using svd2rust)",
             "version": VERSION,
             "authors": AUTHORS,
@@ -51,26 +51,52 @@ def mcu_crate_toml_template(svd_descr: SvdMeta) -> Dict[str, Any]:
     return cargo_toml_mcu_template
 
 
+def pac_readme_template(svd_descr: SvdMeta) -> str:
+    readme_template = rf"""# {svd_descr.name.upper()}
+
+A peripheral access crate for the {svd_descr.name.upper()} from Silabs for Rust Embedded projects.
+
+## License
+
+The included SVD files are sourced from https://www.silabs.com/documents/public/cmsis-packs and
+are licensed under the Zlib (see [LICENSE-3RD-PARTY](LICENSE-3RD-PARTY-Zlib)).
+
+The remainder of the code is under:
+
+- 3-Clause BSD license ([LICENSE-3BSD](LICENSE-3BSD) or https://opensource.org/licenses/BSD-3-Clause)
+
+### Contribution
+
+Unless you explicitly state otherwise, any contribution intentionally submitted for inclusion in the
+work by you, as defined in the BSD-3-Clause license without any additional terms or conditions.
+"""
+
+    return readme_template
+
+
 async def generate_svd2rust_crate(svd_descr: SvdMeta, pac_family: str) -> None:
     print(svd_descr.name, svd_descr.path)
 
     async with CRATE_GEN_SEM:
         with tempfile.TemporaryDirectory() as tmpd:
-            subprocess.run(
-                ["svd2rust", "-i", f"{svd_descr.path}", "-o", f"{tmpd}"],
-                check=True,
+            pret = await asyncio.create_subprocess_exec(
+                *["svd2rust", "-i", f"{svd_descr.path}", "-o", f"{tmpd}"]
             )
+            await pret.wait()
+            assert pret.returncode == 0
             lib_rs = pathlib.Path(tmpd, "lib.rs")
-            subprocess.run(
-                [
+            pret = await asyncio.create_subprocess_exec(
+                *[
                     "form",
                     "-i",
                     f"{lib_rs}",
                     "-o",
                     f"{tmpd}/src",
                 ],
-                check=True,
             )
+
+            await pret.wait()
+            assert pret.returncode == 0
             lib_rs.unlink()
             out_dir = pathlib.Path("pacs", f"{pac_family}", f"{svd_descr.name}")
 
@@ -84,14 +110,26 @@ async def generate_svd2rust_crate(svd_descr: SvdMeta, pac_family: str) -> None:
                 content = mcu_crate_toml_template(svd_descr)
                 toml.dump(content, cargo_toml)
 
-            subprocess.run(["cargo", "fmt"], cwd=out_dir, check=True)
-            subprocess.run(["rustfmt", "build.rs"], cwd=out_dir, check=True)
+            pret = await asyncio.create_subprocess_exec(*["cargo", "fmt"], cwd=out_dir)
+            await pret.wait()
+            assert pret.returncode == 0
+            pret = await asyncio.create_subprocess_exec(
+                *["rustfmt", "build.rs"], cwd=out_dir
+            )
+            await pret.wait()
+            assert pret.returncode == 0
+
+            with out_dir.joinpath("README.md").open("w+") as readme:
+                readme.write(pac_readme_template(svd_descr))
 
 
 def walk_svd_files(svd_dir: Union[str, pathlib.Path]) -> Iterable[SvdMeta]:
     for svd_file in svd_dir.iterdir():
         if svd_file.suffix.endswith("svd"):
-            yield SvdMeta(name=svd_file.stem.lower(), path=svd_file.resolve())
+            yield SvdMeta(
+                name=re.sub(r"f\d+$", "", svd_file.stem.lower()),
+                path=svd_file.resolve(),
+            )
 
 
 async def generate_svd2rust_crates(svd_dir: Union[str, pathlib.Path]) -> None:
@@ -106,13 +144,18 @@ async def generate_svd2rust_crates(svd_dir: Union[str, pathlib.Path]) -> None:
                     asyncio.create_task(generate_svd2rust_crate(svd_file, pac_family))
                 )
 
-    print("await for generation:", len(tasks))
-    time.sleep(10)
     await asyncio.gather(*tasks)
 
 
 def main() -> None:
-    asyncio.run(generate_svd2rust_crates(SVD_DIR))
+    parser = argparse.ArgumentParser(prog="EFM32 Helper Tooling")
+    pacs_parser = parser.add_subparsers(help="Generate PACs", dest="command")
+    pacs_parser.add_parser("pacs-generate", help="Run PACs generation")
+
+    args = parser.parse_args()
+
+    if args.command == "pacs-generate":
+        asyncio.run(generate_svd2rust_crates(SVD_DIR))
 
 
 if __name__ == "__main__":
